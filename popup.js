843be99697c492b80a18080e9e1506364ea86b78
dhanushl0l <dhanushdjtherock@gmail.com>
2023-08-29 02:25:13 +0530

    removed maximize button and add the function to window button

--- popup.js
+++ popup.js
@@ -1,25 +1,18 @@
 document.getElementById("minimizeButton").addEventListener("click", function () {
-    browser.windows.getCurrent().then((windowInfo) => {
-      browser.windows.update(windowInfo.id, { state: "minimized" });
-    });
+  browser.windows.getCurrent().then((windowInfo) => {
+    browser.windows.update(windowInfo.id, { state: "minimized" });
   });
-  
-  document.getElementById("maximizeButton").addEventListener("click", function () {
-    browser.windows.getCurrent().then((windowInfo) => {
-      browser.windows.update(windowInfo.id, { state: "maximized" });
-    });
+});
+
+document.getElementById("closeButton").addEventListener("click", function () {
+  browser.windows.getCurrent().then((windowInfo) => {
+    browser.windows.remove(windowInfo.id);
   });
-  
-  document.getElementById("closeButton").addEventListener("click", function () {
-    browser.windows.getCurrent().then((windowInfo) => {
-      browser.windows.remove(windowInfo.id);
-    });
+});
+
+document.getElementById("WindowButton").addEventListener("click", function () {
+  browser.windows.getCurrent().then((windowInfo) => {
+    const newState = windowInfo.state === "normal" ? "maximized" : "normal";
+    browser.windows.update(windowInfo.id, { state: newState });
   });
-  
-  document.getElementById("WindowButton").addEventListener("click", function () {
-    browser.windows.getCurrent().then((windowInfo) => {
-      const newState = windowInfo.state === "fullscreen" ? "normal" : "fullscreen";
-      browser.windows.update(windowInfo.id, { state: newState });
-    });
-  });
-  
+});
